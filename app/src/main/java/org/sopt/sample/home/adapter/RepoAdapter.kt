907e3a68dd4b4c89b1cf6c77cfc1cb3d909215ae
tama <readyjoo24@gmail.com>
2022-10-13 21:03:14 +0900

    [FEAT] home화면 RecyclerView 구현
    +헤더까지 스크롤 가능하도록

--- app/src/main/java/org/sopt/sample/home/adapter/RepoAdapter.kt
+++ app/src/main/java/org/sopt/sample/home/adapter/RepoAdapter.kt
@@ -3,13 +3,23 @@ package org.sopt.sample.home.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
+import org.sopt.sample.databinding.LayoutRvHeaderBinding
 import org.sopt.sample.databinding.LayoutGithubRepoBinding
 import org.sopt.sample.home.data.Repo
 
-class RepoAdapter(context: Context) : RecyclerView.Adapter<RepoAdapter.RepoViewHolder>() {
+class RepoAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val HEADER = 0
+    private val ITEM = 1
     private val inflater by lazy { LayoutInflater.from(context) }
     private var repoList: List<Repo> = emptyList()
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            HEADER
+        } else ITEM
+    }
 
     class RepoViewHolder(
         private val binding: LayoutGithubRepoBinding
@@ -21,13 +31,35 @@ class RepoAdapter(context: Context) : RecyclerView.Adapter<RepoAdapter.RepoViewH
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
-        val binding = LayoutGithubRepoBinding.inflate(inflater, parent, false)
-        return RepoViewHolder(binding)
+    class HeaderViewHolder(
+        private val binding: LayoutRvHeaderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        val binding = LayoutGithubRepoBinding.inflate(inflater, parent, false)
+//        return RepoViewHolder(binding)
+        return when (viewType) {
+            HEADER -> {
+                val layoutRvHeaderBinding = LayoutRvHeaderBinding.inflate(inflater, parent, false)
+                HeaderViewHolder(layoutRvHeaderBinding)
+            }
+
+            else -> {
+                val githubRepoBinding = LayoutGithubRepoBinding.inflate(inflater, parent, false)
+                RepoViewHolder(githubRepoBinding)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        holder.onBind(repoList[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> {
+//                holder.onBind(repoList[position])
+            }
+            is RepoViewHolder -> {
+                holder.onBind(repoList[position])
+            }
+        }
     }
 
     override fun getItemCount() = repoList.size
