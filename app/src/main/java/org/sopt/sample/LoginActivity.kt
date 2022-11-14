cf7dca4a473816cc6b1d3cc803d229b706d351e8
tama <readyjoo24@gmail.com>
2022-11-14 20:23:42 +0900

    [FEAT] 서버통신 - 로그인

--- app/src/main/java/org/sopt/sample/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/LoginActivity.kt
@@ -4,18 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import org.sopt.sample.databinding.ActivityLoginBinding
+import org.sopt.sample.home.data.remote.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private val authService = ApiFactory.create<AuthService>()
 
-    private var id: String? = null
-    private var password: String? = null
-    private var mbti: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,40 +30,62 @@ class LoginActivity : AppCompatActivity() {
         clickSignup()
     }
 
-    //콜백 선언
+    private fun clickLogin(){
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPw.text.toString()
+            loginEvent(email, password)
+        }
+    }
+    private fun clickSignup() {
+        //회원가입 버튼 클릭 시
+        binding.btnSignup.setOnClickListener {
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+    }
+
+    private fun loginEvent(email: String, password: String) {
+        //로그인 버튼 클릭 시
+        binding.btnLogin.setOnClickListener {
+            val loginService = ServicePool.authService
+            loginService
+                .login(RequestLoginDTO(email, password))
+                .enqueue(object : Callback<ResponseLoginDTO> {
+                    override fun onResponse(
+                        call: Call<ResponseLoginDTO>,
+                        response: Response<ResponseLoginDTO>
+                    ) {
+                        if (response.isSuccessful) {
+                            Log.e("로그인 성공", "login success")
+                            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this@LoginActivity, "Login failed!", Toast.LENGTH_SHORT)
+                                .show()
+                            Log.e("로그인 실패", "login error1")
+                        }
+
+                    }
+
+                    override fun onFailure(call: Call<ResponseLoginDTO>, t: Throwable) {
+                        Log.e("로그인 실패", "login error2")
+                    }
+                }
+                )
+        }
+    }
+
+
     private fun setResultSignUp() {
         resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
+                if (result.resultCode == RESULT_OK) {
                     Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_LONG).show()
-                    val id = result.data?.getStringExtra("id") ?: ""
-                    val password = result.data?.getStringExtra("password") ?: ""
-                    binding.etId.setText(id)
-                    binding.etPw.setText(password)
+                    binding.etEmail.setText(result.data?.getStringExtra("email"))
+                    binding.etPw.setText(result.data?.getStringExtra("password"))
                 }
             }
     }
 
-    private fun clickLogin() {
-        //로그인 버튼 클릭 시
-        binding.btnLogin.setOnClickListener {
-            if (binding.etId.text.length in 6..10 && binding.etPw.text.length in 8..12) {
-                //로그인 성공 시에 HomeActivity 로 이동
-                val intent = Intent(this, HomeActivity::class.java)
-                intent.putExtra("id", id)
-                intent.putExtra("mbti", mbti)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "입력하신 정보를 다시 확인해주세요", Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    private fun clickSignup() {
-        //회원가입 버튼 클릭 시
-        binding.btnSignup.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            resultLauncher.launch(intent)
-        }
-    }
 }
